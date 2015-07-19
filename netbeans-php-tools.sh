de0233e73e1ba4c484bd0875b85749545d91ed8a
Vítězslav Dvořák <vitex@murka.cz>
2015-07-19 04:02:04 +0200

    Version 0.2 package build

--- netbeans-php-tools.sh
+++ netbeans-php-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=`cat package.json | grep '"version":' | awk -F '"' '{ print $4 }'`
+PACKAGE=`cat package.json | grep '"name":' | awk -F '"' '{ print $4 }'`
 if [ -e "build" ]; then
   BUILD=`cat  build | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
 else
@@ -10,7 +11,7 @@ echo $BUILD > build
 
 #CHANGES=`cat CHANGELOG.md | awk -vRS="##" 'NR<=2' ORS="##" | grep -v "##"`
 #dch -v $VERSION-1 --package libjs-twitter-bootstrap $CHANGES
-dch -v $VERSION-$BUILD --package libjs-jquery "Debianized $VERSION"
+dch -v $VERSION-$BUILD --package $PACKAGE "Debianized $VERSION"
 
 debuild -i -us -uc -b
 
