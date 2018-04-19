2d619b67c7f6fe3b8a7b4003c631f52e611b119b
Cybervitexus <info@vitexsoftware.cz>
2018-04-20 01:31:06 +0200

    Download enable

--- netbeans-incubating.sh
+++ netbeans-incubating.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-#wget -c https://builds.apache.org/view/Incubator%20Projects/job/incubator-netbeans-release/lastSuccessfulBuild/artifact/dist/incubating-netbeans-java--bin.zip
+wget -c https://builds.apache.org/view/Incubator%20Projects/job/incubator-netbeans-release/lastSuccessfulBuild/artifact/dist/incubating-netbeans-java--bin.zip
 unzip incubating-netbeans-java--bin.zip
 sudo rm -rf  /usr/local/netbeans-incubating
 sudo mv netbeans /usr/local/netbeans-incubating
