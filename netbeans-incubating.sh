284ed2baf5f60012d022355afd434f2f38c2a138
Vítězslav Dvořák <info@vitexsoftware.cz>
2018-04-20 01:40:43 +0200

    Add run hint

--- netbeans-incubating.sh
+++ netbeans-incubating.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /tmp
-wget -c https://builds.apache.org/view/Incubator%20Projects/job/incubator-netbeans-release/lastSuccessfulBuild/artifact/dist/incubating-netbeans-java--bin.zip
+wget https://builds.apache.org/view/Incubator%20Projects/job/incubator-netbeans-release/lastSuccessfulBuild/artifact/dist/incubating-netbeans-java--bin.zip
 unzip incubating-netbeans-java--bin.zip
 sudo rm -rf  /usr/local/netbeans-incubating
 sudo mv netbeans /usr/local/netbeans-incubating
 rm incubating-netbeans-java--bin.zip
+#run /usr/local/netbeans-incubating/bin/netbeans
